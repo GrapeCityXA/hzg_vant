6b5cb975f03c86d6b028647da2587e2119cdd738
HardyLiu <2569735941@qq.com>
2023-10-11 13:55:18 +0800

    上传代码

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# hzg_vant
-活字格VantUI组件演示demo
+### HZG-VantPlusDemo
+
+Demo体验方式一：推荐移动端扫码
+
+![输入图片说明](https://images.gitee.com/uploads/images/2022/0520/101339_f543f3b5_103682.png "11.png")
+
+Demo体验方式二：直接使用本仓库内代码内容
+
+
+此插件用法跟ElementUI一样，选择你要的组件就行
+
+![输入图片说明](https://images.gitee.com/uploads/images/2022/0520/101350_7944f475_103682.png "222.png")
+
+界面的效果
+
+![输入图片说明](https://images.gitee.com/uploads/images/2022/0520/101507_d5e020a2_103682.png "333.png")
+
+[### 活字格](https://www.grapecity.com.cn/solutions/huozige/introduction)
+
+活字格企业级低代码开发平台，基于葡萄城在专业控件领域 40 年的技术积累，由简单易用的可视化设计器和部署灵活的服务器构成，能帮助开发人员、IT 技术人员和业务人员快速构建美观易用、架构专业、安全可控的企业级多终端应用，并随需而变。活字格高度开放灵活，支持云部署和本地部署，能与微信、钉钉及各行业应用软件无缝集成，并可对接智能硬件、AI 等技术，全面支撑核心业务系统开发。
+
+相比于传统的开发方法，使用活字格可大幅降低应用系统开发的技术门槛，并显著提升开发效率。活字格推动着软件开发方式的革命性变化，引领中国低代码平台发展，曾获中国优秀软件产品、软博会十佳优秀产品等多项荣誉称号。
+
+![输入图片说明](https://www.grapecity.com.cn/images/metalsmith/huozige/index/instruction.png?v=201911051053 "在这里输入图片标题")
---
+++ UserFile/video-swipe.js
@@ -0,0 +1,44 @@
+﻿// JavaScript source code
+
+var FgcVant = FgcVant || {};
+FgcVant.SwipeFeature = FgcVant.SwipeFeature || {};
+FgcVant.SwipeFeature.Item = FgcVant.SwipeFeature.Item || {};
+
+var FgcVant = FgcVant || {};
+FgcVant.SwipeFeature = FgcVant.SwipeFeature || {};
+FgcVant.SwipeFeature.PreviewItem = FgcVant.SwipeFeature.PreviewItem || {};
+
+function insertTemplate(el, id){
+    Forguncy.getTableData("主角表", { "ID": id },(data) => {
+        if (data["是否是视频封面图"] === 1) { // if it's video
+            const icon = document.createElement("div");
+            // draw icon
+            Object.assign(icon.style, {
+                width: "50px",
+                height: "50px",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                zIndex: 1,
+                transform: "translate(-18px, -50%)",
+                background: "url(/Forguncy/GeneratedResources/Images/GenerateImages/ImageCellType/main-circles.png) 0 -55px no-repeat"
+            })
+            // add click listener
+            const onClick = function(event) {
+                window.open("https://videos.grapecity.com.cn/HuoZiGe/Tutorials/v6.0/新手入门视频/新手入门视频-产品介绍.mp4", "_self");
+                event.stopPropagation();
+            }
+            icon.addEventListener("click", onClick);
+            // when preview, the touchend event handler on 'el' will prevent triggering click event on 'icon', so it needs to be stopped when touchend ocurs on 'icon' to avoid triggering touchend handler of 'el'.
+            const stop = function(event) {
+                event.stopPropagation();
+            }
+            icon.addEventListener("touchstart", stop);
+            icon.addEventListener("touchend", stop);
+            // append 'icon' to dom
+            el.appendChild(icon);
+        }	
+    })
+}
+
+FgcVant.SwipeFeature.Item["video-swipe"] = FgcVant.SwipeFeature.PreviewItem["video-swipe"] = insertTemplate;
